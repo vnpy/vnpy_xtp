0bb2a0334e3456401fef3283e420171810cdbad7
vn.py <xiaoyou.chen@foxmail.com>
2021-05-13 12:52:40 +0800

    [Mod] 初始化项目目录

--- README.md
+++ README.md
@@ -1,1 +1,63 @@
-# vnpy_xtp
+# vn.py框架的XTP底层接口
+
+<p align="center">
+  <img src ="https://vnpy.oss-cn-shanghai.aliyuncs.com/vnpy-logo.png"/>
+</p>
+
+<p align="center">
+    <img src ="https://img.shields.io/badge/version-2.2.27.4-blueviolet.svg"/>
+    <img src ="https://img.shields.io/badge/platform-windows|linux-yellow.svg"/>
+    <img src ="https://img.shields.io/badge/python-3.7-blue.svg" />
+    <img src ="https://img.shields.io/github/license/vnpy/vnpy.svg?color=orange"/>
+</p>
+
+## 说明
+
+基于中泰证券XTP柜台的2.2.27.4接口封装开发。
+
+## 安装
+
+安装需要基于2.3.0版本以上的[VN Studio](https://www.vnpy.com)。
+
+直接使用pip命令：
+
+```
+pip install vnpy_xtp
+```
+
+
+或者下载解压后在cmd中运行：
+
+```
+python setup.py install
+```
+
+## 使用
+
+以脚本方式启动（script/run.py）：
+
+```
+from vnpy.event import EventEngine
+from vnpy.trader.engine import MainEngine
+from vnpy.trader.ui import MainWindow, create_qapp
+
+from vnpy_xtp import XtpGateway
+
+
+def main():
+    """主入口函数"""
+    qapp = create_qapp()
+
+    event_engine = EventEngine()
+    main_engine = MainEngine(event_engine)
+    main_engine.add_gateway(XtpGateway)
+    
+    main_window = MainWindow(main_engine, event_engine)
+    main_window.showMaximized()
+
+    qapp.exec()
+
+
+if __name__ == "__main__":
+    main()
+```
---
+++ setup.py
@@ -0,0 +1,66 @@
+import platform
+
+from setuptools import Extension, setup
+
+
+def get_ext_modules() -> list:
+    """
+    获取三方模块
+
+    Linux需要编译封装接口
+    Windows直接使用预编译的pyd即可
+    Mac由于缺乏二进制库支持无法使用
+    """
+    if platform.system() != "Linux":
+        return []
+
+    compiler_flags = [
+        "-std=c++17",
+        "-O3",
+        "-Wno-delete-incomplete", "-Wno-sign-compare",
+    ]
+    extra_link_args = ["-lstdc++"]
+    runtime_library_dirs = ["$ORIGIN"]
+
+    vnxtpmd = Extension(
+        "vnpy_xtp.api.vnxtpmd",
+        [
+            "vnpy_xtp/api/vnxtp/vnxtpmd/vnxtpmd.cpp",
+        ],
+        include_dirs=["vnpy_xtp/api/include",
+                      "vnpy_xtp/api/vnxtp"],
+        define_macros=[],
+        undef_macros=[],
+        library_dirs=["vnpy_xtp/api/libs", "vnpy_xtp/api"],
+        libraries=["thostmduserapi_se", "thosttraderapi_se"],
+        extra_compile_args=compiler_flags,
+        extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
+        depends=[],
+        language="cpp",
+    )
+
+    vnxtptd = Extension(
+        "vnpy_xtp.api.vnxtptd",
+        [
+            "vnpy_xtp/api/vnxtp/vnxtptd/vnxtptd.cpp",
+        ],
+        include_dirs=["vnpy_xtp/api/include",
+                      "vnpy_xtp/api/vnxtp"],
+        define_macros=[],
+        undef_macros=[],
+        library_dirs=["vnpy_xtp/api/libs", "vnpy_xtp/api"],
+        libraries=["thostmduserapi_se", "thosttraderapi_se"],
+        extra_compile_args=compiler_flags,
+        extra_link_args=extra_link_args,
+        runtime_library_dirs=runtime_library_dirs,
+        depends=[],
+        language="cpp",
+    )
+
+    return [vnxtptd, vnxtpmd]
+
+
+setup(
+    ext_modules=get_ext_modules(),
+)
