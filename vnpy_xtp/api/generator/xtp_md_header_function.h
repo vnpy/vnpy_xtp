a119cb2c580a8b06180cfe2c0b596774b9f964fa
noranhe <63988546+noranhe@users.noreply.github.com>
2025-06-11 14:16:16 +0800

    [Del] 可通过generator重新生成的文件

--- vnpy_xtp/api/generator/xtp_md_header_function.h
+++
@@ -1,8 +0,0 @@
-int queryAllTickers(int exchange_id);
-
-int queryTickersPriceInfo(char ticker, int count, int exchange_id);
-
-int queryAllTickersPriceInfo();
-
-int queryAllTickersFullInfo(int exchange_id);
-
